cab58b614481841d0d0e5335f67294b37ae6ebdf
AzriZzz <AzriZzz@users.noreply.github.com>
2021-07-31 15:21:11 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.821bb68abd6a354ed107faa2e8f858ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f654f0fbf3f86f358c9bb74466c385fc",
+    "url": "https://AzriZzz.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "21cbd436374a46f1e50c",
+    "url": "https://AzriZzz.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e2dbfd9d46198dd2dc03",
+    "url": "https://AzriZzz.github.io/task-management-frontend/static/js/2.65b8ab92.chunk.js"
+  },
+  {
+    "revision": "21cbd436374a46f1e50c",
+    "url": "https://AzriZzz.github.io/task-management-frontend/static/js/main.d3a2f4c0.chunk.js"
+  },
+  {
+    "revision": "0084a10a8ed74dd6c108",
+    "url": "https://AzriZzz.github.io/task-management-frontend/static/js/runtime~main.b48a7fa4.js"
+  }
+]);
